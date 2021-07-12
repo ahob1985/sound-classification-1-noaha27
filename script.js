9fc8ce9b2e933cc234bff2f4fc5764c5a412ab0e
nabiy24 <67342736+noaha27@users.noreply.github.com>
2021-07-12 18:29:07 +0000

    first commit

--- script.js
+++ script.js
@@ -11,15 +11,36 @@ let textP2;
 let soundClassifier;
 
 function setup() {
-
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  textDiv = createDiv();
+  textP = createP("Model loading, please...");
+  textP.parent(textDiv);
+  textP2 = createP();
+  textP2.parent(textDiv);
+  let options = {
+    probalityTreshold: 0.95
+  };
+  soundClassifier = ml5,soundClassifier("SpeechCommands18w", options,modelReady);
 }
 
 function draw() {
-
+  let label = textP.html();
+  if(label.includes("up")) {
+    background(255, 0, 0);
+  } else if(label.includes("down")) {
+    background(0, 0, 255);
+  } else if(label.includes("left")) {
+    background(0, 255, 0);
+  } else if(label.includes("right")) {
+    background(0, 255, 255);
+  }
 }
 
 function modelReady() {
-
+  textP.html("Model loaded. Say any of the commands below!");
+  textP2.html("<b>Commands</b>: digits zero through nine, up, down, left right, go, stop, yes, no");
+  soundClassifier.classify(gotResults);
 }
 
 function gotResults(error, results) {
@@ -27,8 +48,9 @@ function gotResults(error, results) {
     console.error(error);
   } else {
     // Complete the code below
-    let label;
-    let confidence;
+    let label = results[0].label;
+    let confidence = results[0].confidence, 2) * 100;
+    textP.html(label + ":" + confidence + "%");
 
   }
 }
